e246c38a98495bf40442975227b938af09b3fb77
it <ityeri9@gmail.com>
2026-03-03 15:25:16 +0900

    wasans

--- src/cherylog/__init__.py
+++ src/cherylog/__init__.py
@@ -4,10 +4,17 @@ from cherylog import common
 from cherylog import units
 from cherylog import log_formatter
 
+def main():
+    container = bootstrap.Container()
+    bootstrapper = bootstrap.Bootstrapper(container)
+    bootstrapper.run()
+
 __all__ = [
     'bootstrap',
     'config_provider',
     'common',
     'units',
-    'log_formatter'
+    'log_formatter',
+
+    'main'
 ]
--- src/cherylog/__main__.py
+++ src/cherylog/__main__.py
@@ -1,10 +1,4 @@
-from cherylog.bootstrap import Container, Bootstrapper
-
-container = Container()
-bootstrapper = Bootstrapper(container)
-
-def main():
-    bootstrapper.run()
+from cherylog import main
 
 if __name__ == '__main__':
     main()
